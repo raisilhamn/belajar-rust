f6f498e50bb96a8c9aa5c422956ecca317169d22
Rais Ilham <raispinter12@gmail.com>
2022-05-15 20:54:05 +0700

    final code

--- 1_Getting_Started/guessing_game/src/main.rs
+++ 1_Getting_Started/guessing_game/src/main.rs
@@ -1,34 +1,49 @@
-use std::io;
 use rand::Rng;
-
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     let x = 5;
     let y = 10;
     println!("x = {} and y = {}", x, y);
-    
+
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..101);
 
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
-    // We use the let statement to create the variable. Here’s another example:
-    // let apples = 5; // immutable
-    // let mut bananas = 5; // mutable
-    // new = membuat instance kosong bertipe string 
-    
-    io::stdin()
-        .read_line(&mut guess)
-        // The & indicates that this argument is a reference, 
-        // which gives you a way to let multiple parts of your code access one piece of data
-        //  without needing to copy that data into memory multiple times. 
-        .expect("Failed to read line");
-        // Bisa juga ditulis dengan 
+        let mut guess = String::new();
+        // We use the let statement to create the variable. Here’s another example:
+        // let apples = 5; // immutable
+        // let mut bananas = 5; // mutable
+        // new = membuat instance kosong bertipe string
+
+        io::stdin()
+            .read_line(&mut guess)
+            // The & indicates that this argument is a reference,
+            // which gives you a way to let multiple parts of your code access one piece of data
+            //  without needing to copy that data into memory multiple times.
+            .expect("Failed to read line");
+        // Bisa juga ditulis dengan
         // io::stdin().read_line(&mut guess).expect("Failed to read line");
-    println!("You guessed: {}", guess);
-    // io::stdout().flush();
 
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+        // io::stdout().flush();
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+                        Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
