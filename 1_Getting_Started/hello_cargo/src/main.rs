bee2b8d1c48898bc5f4d72ab7911dee4c45cad5a
Rais Ilham <raispinter12@gmail.com>
2022-05-15 19:59:17 +0700

    cargo

---
+++ 1_Getting_Started/hello_cargo/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("Hello, world!2");
+}
+// We can build a project using "cargo build".
+// We can build and run a project in one step using "cargo run".
+// We can build a project without producing a binary to check for errors using "cargo check".
