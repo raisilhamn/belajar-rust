9ebd668bd37f0af31f9a4e104f8791cdf7750373
Rais Ilham <raispinter12@gmail.com>
2022-05-15 20:55:05 +0700

    final code

--- 1_Getting_Started/guessing_game/src/main.rs
+++ 1_Getting_Started/guessing_game/src/main.rs
@@ -34,6 +34,7 @@ fn main() {
             Ok(num) => num,
             Err(_) => continue,
         };
+        // let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
         println!("You guessed: {}", guess);
         // io::stdout().flush();
